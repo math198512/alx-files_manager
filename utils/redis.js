f167638a242187288875528e571f080961a3c782
math198512 <thoraichi@gmail.com>
2024-12-08 18:03:51 +0100

    ./utils/redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -34,14 +34,7 @@ class RedisClient {
    * @returns {String | Object}
    */
   async get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, reply) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(reply);
-      });
-    });
+    return promisify(this.client.GET).bind(this.client)(key);
   }
 
   /**
