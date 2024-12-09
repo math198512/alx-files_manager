33df2e55eee715826a9fd0842da18707de030dbe
math198512 <thoraichi@gmail.com>
2024-12-09 14:19:30 +0100

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+import mapRoutes from './routes';
+
 const express = require('express');
-const routes = require('./routes');
 
 // Create Express application
 const app = express();
@@ -7,10 +8,11 @@ const app = express();
 // Get port from environment variable or use default 5000
 const PORT = process.env.PORT || 5000;
 
-// Load routes
-app.use('/', routes);
+mapRoutes(app);
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
