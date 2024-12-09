6cc28ecbfe34e1e8058d5d85279bf53360f18b89
math198512 <thoraichi@gmail.com>
2024-12-09 15:03:08 +0100

    ./controllers/AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,32 +1,18 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-class AppController {
-  // Handler for GET /status endpoint
-  static async getStatus(req, res) {
-    // Check Redis and DB client connection status
-    const redisAlive = await redisClient.isAlive();
-    const dbAlive = await dbClient.isAlive();
-
-    // Return status with connection information
-    return res.status(200).json({
-      redis: redisAlive,
-      db: dbAlive,
+export default class AppController {
+  static getStatus(req, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     });
   }
 
-  // Handler for GET /stats endpoint
-  static async getStats(req, res) {
-    // Count users and files in the database
-    const usersCount = await dbClient.nbUsers();
-    const filesCount = await dbClient.nbFiles();
-
-    // Return statistics
-    return res.status(200).json({
-      users: usersCount,
-      files: filesCount,
-    });
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
   }
 }
-
-module.exports = AppController;
