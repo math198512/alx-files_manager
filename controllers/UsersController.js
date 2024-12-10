b1617e02baddc534f42e00cf01f613e13c06bbed
math198512 <thoraichi@gmail.com>
2024-12-10 15:34:37 +0100

    controllers/UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,5 @@
+import sha1 from 'sha1';
 import dbClient from '../utils/db';
-import sha1 from 'sha1'
-
 
 const postNew = async (req, res) => {
   const email = req.body ? req.body.email : null;
@@ -14,16 +13,17 @@ const postNew = async (req, res) => {
     res.status(400).json({ error: 'Missing password' });
     return;
   }
-
+  const user = await (await dbClient.usersCollection()).findOne({ email });
 
   if (user) {
     res.status(400).json({ error: 'Already exist' });
     return;
   }
-  const insertionInfo = await dbClient.insertOne('users', { email, password: sha1(password) });
+  const insertionInfo = await (await dbClient.usersCollection())
+    .insertOne({ email, password: sha1(password) });
   const userId = insertionInfo.insertedId.toString();
 
   res.status(201).json({ email, id: userId });
-}
+};
 
 module.exports = { postNew };
