ea64f47aa5d9d1ef07b2356b10ef68e2f46cb7dd
math198512 <thoraichi@gmail.com>
2024-12-10 09:50:21 +0100

    .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,6 @@ module.exports = {
         'max-classes-per-file': 'off',
         'no-underscore-dangle': 'off',
         'no-console': 'off',
-        'import/no-named-as-default': 0,
         'no-shadow': 'off',
         'no-restricted-syntax': [
             'error',
