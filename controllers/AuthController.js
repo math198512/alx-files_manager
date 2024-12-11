6c5849a8401e769d6b96d535ade69a0274589fa1
math198512 <thoraichi@gmail.com>
2024-12-11 22:48:52 +0100

    controllers/AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,9 +1,19 @@
-import sha1 from 'sha1';
+import { v4 as uuidv4 } from 'uuid';
 import dbClient from '../utils/db';
 
-const getConnect = (req, res) => {
-  const { authorization } = req;
-  res.status(200).json({ 'authorization': authorization });;
-};
+const getConnect = async (req, res) => {
+  const { user } = req;
+  const token = uuidv4();
 
-module.exports = { getConnect };
+  await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+  res.status(200).json({ token });
+}
+
+const getDisconnect = async (req, res) => {
+  const token = req.headers['x-token'];
+
+  await redisClient.del(`auth_${token}`);
+  res.status(204).send();
+}
+
+module.exports = { getConnect, getDisconnect };
