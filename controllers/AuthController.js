9d97fad1fa2100ededec9304b744be423e9694d0
math198512 <thoraichi@gmail.com>
2024-12-11 21:32:08 +0100

    controllers/AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,7 +2,7 @@ import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
 const getConnect = async (req, res) => {
-  const { authorization } = await req.headers.authorization;
+  const { authorization } = await req;
   console.log(authorization);
 };
 
