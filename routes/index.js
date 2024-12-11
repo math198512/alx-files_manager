689ebec48ac075868cad50fe7d490710eb1070ea
math198512 <thoraichi@gmail.com>
2024-12-11 18:48:57 +0100

    routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
@@ -9,5 +10,6 @@ const router = Router();
 router.get('/stats', AppController.getStats);
 router.get('/status', AppController.getStatus);
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
 
 export default router;
