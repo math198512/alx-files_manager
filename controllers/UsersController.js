818a6167a1d81a2a5e2d6248a4cb75c4f508c449
math198512 <thoraichi@gmail.com>
2024-12-12 11:39:13 +0100

    controllers/UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 const postNew = async (req, res) => {
   const email = req.body ? req.body.email : null;
