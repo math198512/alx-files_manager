c3efa331570fd39070559e2f3e5453c0c4dedb36
math198512 <thoraichi@gmail.com>
2024-12-13 12:35:51 +0100

    controllers/FilesController.js

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -8,7 +8,9 @@ const postUpload = async (req, res) => {
   if (userId) {
     const user = await (await dbClient.usersCollection()).findOne({ _id: ObjectId(userId) });
     if (user) {
-      return res.status(200).json({ id: user._id, email: user.email });
+
+
+      console.log(user);
     }
   }
   return res.status(401).json({ error: 'Unauthorized' });
